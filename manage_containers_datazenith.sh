f495c078257c59c8d5f4c7c3413c34768697f132
Cesar Trujillo <cesar.trujillo@amovil.co>
2024-06-28 23:22:43 -0500

    cambio en los tiempos de espera de javascript

---
+++ manage_containers_datazenith.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Detener contenedores específicos
+docker stop datazenith_rqworker_1 datazenith_web_1 datazenith_redis_1
+
+# Eliminar contenedores específicos
+docker rm datazenith_rqworker_1 datazenith_web_1 datazenith_redis_1
+
+# Limpiar el sistema Docker sin confirmación
+docker system prune -f
+
+# Navegar al directorio del proyecto
+cd /var/www/datazenith
+
+# Eliminar la base de datos SQLite
+rm -f mydata.db
+
+# Activar el entorno virtual de Python
+source venv/bin/activate
+
+# Levantar los servicios con docker-compose
+docker-compose -f docker-compose.rq.yml up -d --build
